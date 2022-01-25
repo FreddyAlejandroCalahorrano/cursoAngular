d1b95fc7060fa7ff65d6a781d19b01c65d2b7e4f
Freddy_Calahorrano <freddy.calahorrano01@epn.edu.ec>
2022-01-24 22:32:44 -0500

    Segundo Commit Comunicacion entre componentes

---
+++ src/app/acumulador/acumulador.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-acumulador',
+  templateUrl: './acumulador.component.html',
+  styleUrls: ['./acumulador.component.scss']
+})
+export class AcumuladorComponent implements OnInit {
+
+  @Input() contador: number = 0;
+  @Input('acm') acumulador: number = 0;
+
+  @Output() evtClick: EventEmitter<string> = new EventEmitter<string>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  changevalue(valor:number){
+    this.contador = this.contador + valor
+
+    this.evtClick.emit(`Incremento -o- Decremento: ${valor}`)
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { env } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  nombreEstudiante = 'Freddy Calahorrano';
+  edadEstudiante = 28;
+
+  horaActual: Date;
+
+  colors: Icolor[] = [
+    {redSocial: 'Whatsapp', color: '#25D366'},
+  ];
+
+  logAcumulador: string[] = [];
+
+  insertLogAcumulador(log: string){
+    console.log(log);
+    this.logAcumulador.push(log);
+  }
+
+  mostrarHora(evento: any){
+    console.log(evento)
+    this.horaActual = new Date();
+    const {target} = evento;
+    target.innerHtml = `Cambio Hora - ${this.horaActual.getSeconds()}`
+
+    this.horaActual = new Date()
+  }
+}
+
+interface Icolor {
+  
 }
